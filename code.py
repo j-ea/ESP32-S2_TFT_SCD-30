57674f9f0f5bba30a1e5e7686efa07d480638a9a
j-ea <32227134+j-ea@users.noreply.github.com>
2022-08-01 19:23:37 -0400

    Add colors corresponding to desirability of output

--- code.py
+++ code.py
@@ -8,19 +8,61 @@ from adafruit_display_text import bitmap_label
 
 i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
 scd = adafruit_scd30.SCD30(i2c)
-text_area = bitmap_label.Label(terminalio.FONT, scale=2)
-text_area.anchor_point = (0.5, 0.5)
-text_area.anchored_position = (board.DISPLAY.width // 2, board.DISPLAY.height // 2)
-
+display = board.DISPLAY
 main_group = Group()
 
-main_group.append(text_area)
+green=0x00ff00
+yellow=0xffff00
+red=0xff0000
 
-print("Let Go!")
+co2_text = bitmap_label.Label(terminalio.FONT, scale=2)
+co2_text.anchor_point = (0.0, 2.5)
+co2_text.anchored_position = (0, display.height // 2)
 
-board.DISPLAY.show(main_group)
+temp_text = bitmap_label.Label(terminalio.FONT, scale=2)
+temp_text.anchor_point = (0.0, 0.5)
+temp_text.anchored_position = (0, display.height // 2)
+
+rh_text = bitmap_label.Label(terminalio.FONT, scale=2)
+rh_text.anchor_point = (0.0, -1.3)
+rh_text.anchored_position = (0, display.height // 2)
+
+main_group.append(co2_text)
+main_group.append(temp_text)
+main_group.append(rh_text)
+
+print("Let's go!")
+print(display.width)
+print(display.height)
+
+display.show(main_group)
 
 while True:
     if scd.data_available:
-        text_area.text = "CO2: {} PPM \nTemp: {:.1f} C \nHumidity {:.1f} % rH".format(scd.CO2, scd.temperature, scd.relative_humidity)
+        if scd.CO2 <= 800:
+            co2_text.color = green
+        elif scd.CO2 > 800 and scd.CO2 <= 1200:
+            co2_text.color = yellow
+        else:
+            co2_text.color = red
+        
+        if scd.temperature >= 16 and scd.temperature <= 26:
+            temp_text.color = green
+        elif scd.temperature > 12 and scd.temperature < 16:
+            temp_text.color = yellow
+        elif scd.temperature > 26 and scd.temperature < 30:
+            temp_text.color = yellow
+        else:
+            temp_text.color = red
+        
+        if scd.relative_humidity >= 75:
+            rh_text.color = green
+        elif scd.relative_humidity > 60 and scd.relative_humidity < 75:
+            rh_text.color = yellow
+        else:
+            rh_text.color = red
+        
+        co2_text.text = "CO2: {} PPM".format(scd.CO2)
+        temp_text.text = "Temp: {:.1f}".format(scd.temperature)
+        rh_text.text = "Humidity {:.1f} % rH".format(scd.relative_humidity)
     time.sleep(0.5)
